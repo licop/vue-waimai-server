58b761d13a217218405a9fe4e32e4a1d05f0a051
licop <licop@users.noreply.github.com>
2023-03-30 17:26:41 +0800

    增加搜索接口

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 // 处理自定义路由
 
 const test = require('./controller/test')
+const home_search = require('./controller/home_search')
 
 module.exports = (app) => {
   app.use('/api/test', test)
+  app.use('/api/home_search', home_search)
 }
